a87f9809575b1fa31dd727737faec88eff3a9228
Julien Le Bren <70627850+julienlebren@users.noreply.github.com>
2023-09-28 09:06:56 +0200

    Replaced `window` with new syntax since window is deprecated (#7)

--- lib/src/localization/app_localizations_provider.dart
+++ lib/src/localization/app_localizations_provider.dart
@@ -6,10 +6,10 @@ import 'dart:ui' as ui;
 /// provider used to access the AppLocalizations object for the current locale
 final appLocalizationsProvider = Provider<AppLocalizations>((ref) {
   // set the initial locale
-  ref.state = lookupAppLocalizations(basicLocaleListResolution([ui.window.locale], AppLocalizations.supportedLocales));
+  ref.state = lookupAppLocalizations(basicLocaleListResolution([WidgetsBinding.instance.platformDispatcher.locale], AppLocalizations.supportedLocales));
   // update afterwards
   final observer = _LocaleObserver((locales) {
-    ref.state = lookupAppLocalizations(basicLocaleListResolution([ui.window.locale], AppLocalizations.supportedLocales));
+    ref.state = lookupAppLocalizations(basicLocaleListResolution([WidgetsBinding.instance.platformDispatcher.locale], AppLocalizations.supportedLocales));
   });
   final binding = WidgetsBinding.instance;
   binding.addObserver(observer);
